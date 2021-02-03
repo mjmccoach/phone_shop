659df517d8b9f4798fa69494642208d393906309
Matthew <m.mccoach88@gmail.com>
2021-02-03 17:58:37 +0000

    Created an array of items

--- src/containers/DisplayBox.js
+++ src/containers/DisplayBox.js
@@ -2,6 +2,31 @@ import PhoneList from '../components/PhoneList'
 
 const DisplayBox = () => {
 
+    const [phones, setPhones] = useState([]);
+
+    const phones = [
+        {
+            size: "50gb",
+            color: "Red"
+        },
+        {
+            size: "50gb",
+            color: "Blue"
+        },
+        {
+            size: "50gb",
+            color: "Pink"
+        },
+        {
+            size: "100gb",
+            color: "Red"
+        },
+        {
+            size: "100gb",
+            color: "Blue"
+        }
+    ]
+
     return(
         <>
         <h1>hiya</h1>
